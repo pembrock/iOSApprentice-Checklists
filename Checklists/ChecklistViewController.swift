35b042aa4e291964bf310630cf38cca1a5367b75
Kirill Pasikuta <pembrock@gmail.com>
2019-12-04 17:30:57 +0300

    show cell in table view

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -15,6 +15,15 @@ class ChecklistViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK:- Tab;e View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 
 }
 
