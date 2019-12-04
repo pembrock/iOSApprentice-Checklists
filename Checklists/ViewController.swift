fd8eff2c912fc8c78a9d02c782845c18a0f5160b
Kirill Pasikuta <pembrock@gmail.com>
2019-12-04 14:09:13 +0300

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Pasikuta Kirill on 04.12.2019.
+//  Copyright © 2019 Pasikuta Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
