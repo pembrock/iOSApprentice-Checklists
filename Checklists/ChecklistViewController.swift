2a5c356abd62377fd74099f6b7987c1de1a374ac
Kirill Pasikuta <pembrock@gmail.com>
2019-12-10 23:07:04 +0300

    data rows in constants

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
 
+    let row0text = "Walk the dog"
+    let row1text = "Brush my teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Socсer practice"
+    let row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,7 +29,7 @@ class ChecklistViewController: UITableViewController {
 
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -26,15 +38,15 @@ class ChecklistViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         
         if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row % 5 == 3 {
-            label.text = "Socсer practice"
+            label.text = row3text
         } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+            label.text = row4text
         }
         
         return cell
