da70fcd1b573efe189e02dce4249fa82f9bb7ebf
Kirill Pasikuta <pembrock@gmail.com>
2020-02-11 17:59:15 +0300

    loading data

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -51,32 +51,8 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
         
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let item1 = ChecklistItem()
-        item1.text = "Walk the dog"
-        items.append(item1)
-        
-        let item2 = ChecklistItem()
-        item2.text = "Brush my teeth"
-        item2.checked = true
-        items.append(item2)
-        
-        let item3 = ChecklistItem()
-        item3.text = "Learn iOS development"
-        item3.checked = true
-        items.append(item3)
-        
-        let item4 = ChecklistItem()
-        item4.text = "Soccer practice"
-        items.append(item4)
-        
-        let item5 = ChecklistItem()
-        item5.text = "Eat ice cream"
-        items.append(item5)
-        
-        let item6 = ChecklistItem()
-        item6.text = "Read book"
-        items.append(item6)
-        
+        // Load items
+        loadChecklistItems()
         print("Documents folder is \(documentsDirectory())")
         print("Data file path is \(dataFilePath())")
     }
@@ -160,6 +136,18 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
                            options: Data.WritingOptions.atomic)
         } catch {
             print("Error encoding item array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklistItems() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                items = try decoder.decode([ChecklistItem].self, from: data)
+            } catch {
+                print("Error decoding item array: \(error.localizedDescription)")
+            }
         }
     }
 
