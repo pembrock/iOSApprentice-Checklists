72ebd2168fda398c3d59514293326227d2d9dc2e
Kirill Pasikuta <pembrock@gmail.com>
2020-02-12 23:22:58 +0300

    all checklists screen

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Евгения Пупликова on 12.02.2020.
+//  Copyright © 2020 Pasikuta Kirill. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+
+    let cellIdentifier = "ChecklistCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+}
